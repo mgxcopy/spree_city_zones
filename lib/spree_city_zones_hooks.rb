10dc517170d7506bbeddfcbe26b373c3b9fd9974
johncox00 <john@mach19.com>
2014-07-10 11:14:24 -0500

    updates to use deface

--- lib/spree_city_zones_hooks.rb
+++ lib/spree_city_zones_hooks.rb
@@ -1,4 +1,10 @@
-class SpreeCityZonesHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
-  insert_after :admin_configurations_menu, 'shared/cities_admin_configurations_menu'
-end
+Deface::Override.new(:virtual_path  => "store/admin/configurations/index",
+                     :name          => "city_zones",
+                     :insert_bottom => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :text          => "<%= configurations_menu_item('City Zones', admin_cities_path, 'Manage Cities' %>")
+
+
+# class SpreeCityZonesHooks < Spree::ThemeSupport::HookListener
+#   # custom hooks go here
+#   insert_after :admin_configurations_menu, 'shared/cities_admin_configurations_menu'
+# end
