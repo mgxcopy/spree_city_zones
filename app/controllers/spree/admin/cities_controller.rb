156d7d9aa60d4d9b8d4afcfdc6c5adb2c6a28406
johncox00 <john@mach19.com>
2014-07-17 21:36:55 -0600

    jkhaflds

--- app/controllers/spree/admin/cities_controller.rb
+++ app/controllers/spree/admin/cities_controller.rb
@@ -1,6 +1,7 @@
 module Spree
   module Admin
     class CitiesController < Spree::Admin::ResourceController
+      State = Spree::State.class
       belongs_to :state, class_name: 'Spree::State'
       before_filter :load_data
 
