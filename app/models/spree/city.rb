59782d2e909c50464ba7c509dc2d285763ef7fde
johncox00 <john@mach19.com>
2014-07-17 21:46:16 -0600

    jkhaflds

--- app/models/spree/city.rb
+++ app/models/spree/city.rb
@@ -1,9 +1,9 @@
 module Spree
   class City < ActiveRecord::Base
-    has_one :spree_zone_member, :as => :zoneable
-    has_one :spree_zone, :through => :spree_zone_member
-    belongs_to :spree_state
-    has_one :spree_country, :through => :spree_state
+    has_one :zone_member, :as => :zoneable, class_name: 'Spree::ZoneMember'
+    has_one :zone, :through => :spree_zone_member, class_name: 'Spree::Zone'
+    belongs_to :state, class_name: 'Spree::State'
+    has_one :country, :through => :state, class_name: 'Spree::Country'
     validates_presence_of :name
     validates_uniqueness_of :name, :scope => [:state_id]
     
