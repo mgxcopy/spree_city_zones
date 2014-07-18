b97018f6d4cbab49e9d98180042144cfc8da352d
johncox00 <john@mach19.com>
2014-07-17 20:21:11 -0600

    more playing

---
+++ app/models/spree/country_decorator.rb
@@ -0,0 +1,3 @@
+Spree::Country.class_eval do
+  has_many :cities, :through => :states
+end
---
+++ app/models/spree/zone_decorator.rb
@@ -0,0 +1,53 @@
+Spree::Zone.class_eval do
+  def include?(address)
+    return false unless address
+
+    # NOTE: This is complicated by the fact that include? for HMP is broken in Rails 2.1 (so we use awkward index method)
+    members.any? do |zone_member|
+      case zone_member.zoneable_type
+      when "Zone"
+        zone_member.zoneable.include?(address)
+      when "Country"
+        zone_member.zoneable == address.country
+      when "City"
+        zone_member.zoneable == City.find(:first, :conditions => ['UPPER(name) = :name and state_id = :state_id', {:name => address.city.upcase, :state_id => address.state.id}])
+        #Break out so State Zones aren't also found for taxation
+        #this allows a default state tax if no city found.... probably breaks shipping calculators though?!?!
+        break
+      when "State"
+        zone_member.zoneable == address.state
+      else
+        false
+      end
+    end
+  end
+
+  def kind
+    member = self.members.last
+    case member && member.zoneable_type
+    when "City"   then "city"
+    when "State"  then "state"
+    when "Zone"   then "zone"
+    else
+      "country"
+    end
+  end
+
+  def country_list
+    members.map {|zone_member|
+      case zone_member.zoneable_type
+      when "Zone"
+        zone_member.zoneable.country_list
+      when "Country"
+        zone_member.zoneable
+      when "State"
+        zone_member.zoneable.country
+      when "City"
+        zone_member.zoneable.country_list
+      else
+        nil
+      end
+    }.flatten.compact.uniq
+  end
+  
+end
