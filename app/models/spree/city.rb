bf9ef44400b36a8f5d9ca497a805b400c7bd8343
Adnan Abdulally <adnan@mgxcopy.com>
2015-10-01 15:56:32 -0700

    updated state for strong_parameters gem

--- app/models/spree/city.rb
+++ app/models/spree/city.rb
@@ -7,6 +7,8 @@ module Spree
     validates_presence_of :name
     validates_uniqueness_of :name, :scope => [:state_id]
     
+    attr_accessible :name, :state, :state_id
+    
     def state_and_city
       if state
         state.name + '- ' + name
